d1a9f317ee09b1d428e24d29f0467b32da4ed5a4
mafefreeitas <128387890+mafefreeitas@users.noreply.github.com>
2023-05-27 20:00:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ def count_vowels(string:str) -> int:
     Returns:
         int: The number of vowels in the string.
     """
+"""This program counts the number of vowels in a string."""
+    vogais = "aeiou"  
+    resultado = 0 
+
+    for caractere in string.lower():
+        if caractere in vogais:
+            resultado += 1
+
+    return resultado
+
+palavra = input("Digite uma palavra qualquer: ")
+numero_vogais = count_vowels(palavra)
+print("O numero de vogais é:", numero_vogais)
